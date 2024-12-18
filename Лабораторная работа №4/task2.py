1fea047f2f758bcc92315b4535e456c5c9f9cf7c
ShirokovaPolina <pdsirokova@gmail.com>
2024-12-19 01:18:42 +0300

    task2.py

--- Лабораторная работа №4/task2.py
+++ Лабораторная работа №4/task2.py
@@ -6,11 +6,10 @@ OUTPUT_FILENAME = "output.json"
 
 
 def task() -> None:
-    data = []
     with open(INPUT_FILENAME, 'r', encoding='utf-8') as csvfile:
         reader = csv.DictReader(csvfile)
-        for row in reader:
-            data.append(row)
+        data = [row for row in reader]  # Используем list comprehension для создания списка
+
     json_string = json.dumps(data, indent=4)
     with open(OUTPUT_FILENAME, 'w', encoding='utf-8') as outfile:
         outfile.write(json_string)
@@ -22,3 +21,4 @@ if __name__ == '__main__':
     with open(OUTPUT_FILENAME) as output_f:
         for line in output_f:
             print(line, end="")
+            
